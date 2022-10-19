3960dc210548e854612634517b6185be5bde05aa
devhum7p <devhum7p@gmail.com>
2022-10-19 20:23:50 +0900

    signup (POST)

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def signup(request):
-    form = UserCreationForm()
 
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("article:index")
+    else:
+        form = UserCreationForm()
     context = {
         "form": form,
     }
