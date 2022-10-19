4b84026242e1d4bf1c032a6d3dba8f9f1b1d0fef
devhum7p <devhum7p@gmail.com>
2022-10-19 17:04:48 +0900

    update (POST)

--- articles/views.py
+++ articles/views.py
@@ -46,8 +46,13 @@ def update(request, article_pk):
 
     article = Article.objects.get(pk=article_pk)
 
-    article_form = ArticleForm(instance=article)
-
+    if request.method == "POST":
+        article_form = ArticleForm(request.POST, instance=article)
+        if article_form.is_valid():  # 유효성 검사
+            article_form.save()  # DB 저장 로직
+            return redirect("articles:detail", article.pk)
+    else:
+        article_form = ArticleForm(instance=article)
     context = {
         "article_form": article_form,
     }
