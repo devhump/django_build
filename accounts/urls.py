a8c7d8ff8f24ba78950991932202d3b4345c58c2
devhum7p <devhum7p@gmail.com>
2022-10-21 00:42:44 +0900

    bug-fixed : accounts-detail & update

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,12 +4,12 @@ from . import views
 app_name = "accounts"
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    # path("", views.index, name="index"),
     path("signup/", views.signup, name="signup"),
     path("login/", views.login, name="login"),
     path("logout/", views.logout, name="logout"),
-    path("<int:user_pk>/detail/", views.detail, name="detail"),
-    path("<int:user_pk>/update/", views.update, name="update"),
+    path("detail/", views.detail, name="detail"),
+    path("update/", views.update, name="update"),
     path("password/", views.change_password, name="change_password"),
     path("delete/", views.delete, name="delete"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -58,37 +58,37 @@ def index(request):
     return render(request, "accounts/index.html", context)
 
 
-def detail(request, user_pk):
+def detail(request):
 
-    user = get_user_model().objects.get(pk=user_pk)
+    # user = get_user_model().objects.get(pk=user_pk)
 
     context = {
-        "user": user,
+        "user": request.user,
     }
     return render(request, "accounts/detail.html", context)
 
 
-# 관리자가 회원 정보 확인할 때
-def update(request, user_pk):
+# # 관리자가 회원 정보 확인할 때
+# def update(request, user_pk):
 
-    user = get_user_model().objects.get(pk=user_pk)
+#     user = get_user_model().objects.get(pk=user_pk)
 
-    if request.method == "POST":
-        form = CustomUserChangeForm(request.POST, instance=user)
-        # form = CustomUserChangeForm(data=request.POST, instance=user)
-        if form.is_valid():
-            form.save()
-            return redirect("accounts:detail", user.pk)
-    else:
-        form = CustomUserChangeForm(instance=user)
-    context = {
-        "form": form,
-    }
-    return render(request, "accounts/update.html", context)
+#     if request.method == "POST":
+#         form = CustomUserChangeForm(request.POST, instance=user)
+#         # form = CustomUserChangeForm(data=request.POST, instance=user)
+#         if form.is_valid():
+#             form.save()
+#             return redirect("accounts:detail", user.pk)
+#     else:
+#         form = CustomUserChangeForm(instance=user)
+#     context = {
+#         "form": form,
+#     }
+#     return render(request, "accounts/update.html", context)
 
 
 # 로그인한 유저의 본인 정보 수정
-def update2(request):
+def update(request):
 
     if request.method == "POST":
         form = CustomUserChangeForm(request.POST, instance=request.user)
